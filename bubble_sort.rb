0fe3c3f6e0bf58d9d739b15c2e31a98e4832f885
dev <dev@localhost.localdomain>
2020-10-01 00:39:34 +0300

    add swaping of elements in the array

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -11,5 +11,13 @@ def bubble_sort(array_to_sort)
 
         # subtract one because Ruby arrays are zero-index based
         (array_length-1).times do |i|
+            if array_to_sort[i] > array_to_sort[i+1]
+                array_to_sort[i], array_to_sort[i+1] = array_to_sort[i+1], array_to_sort[i]
+                swapped = true
+            end
+
+            break if not swapped
     end
+    return array_to_sort
 end
+
