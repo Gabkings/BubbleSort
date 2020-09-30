05669ae4031c3c09f484c1e766bf50cf7dca69b4
dev <dev@localhost.localdomain>
2020-10-01 02:20:24 +0300

    fixed rubocop errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,39 +1,45 @@
-# Take array as a parameter
+# rubocop: disable Metrics/MethodLength
+# rubocop: disable Style/FrozenStringLiteralComment
 
 def bubble_sort(array_to_sort)
-    array_length = array_to_sort.size
-    return array if array_length <= 1
-    # create a loop to loop through array and compare elements
-    loop do
-        swapped = false
-        # subtract one because Ruby arrays are zero-index based
-        (array_length-1).times do |i|
-            if array_to_sort[i] > array_to_sort[i+1]
-                array_to_sort[i], array_to_sort[i+1] = array_to_sort[i+1], array_to_sort[i]
-                swapped = true
-            end
-        end
-        break if not swapped 
+  array_length = array_to_sort.size
+  return array if array_length <= 1
+
+  # create a loop to loop through array and compare elements
+  loop do
+    swapped = false
+    # subtract one because Ruby arrays are zero-index based
+    (array_length - 1).times do |i|
+      if array_to_sort[i] > array_to_sort[i + 1]
+        array_to_sort[i], array_to_sort[i + 1] = array_to_sort[i + 1], array_to_sort[i]
+        swapped = true
+      end
     end
-    puts array_to_sort
+    break unless swapped
+  end
+  puts array_to_sort
 end
 
 def bubble_sort_by(sorted_array)
-    check = true
-    while check
-      check = false
-      (0...sorted_array.length - 1).each do |i|
-        next unless yield(sorted_array[i], sorted_array[i + 1]).positive?
-        sorted_array[i], sorted_array[i + 1] = sorted_array[i + 1], sorted_array[i]
-        check = true
-      end
+  check = true
+  while check
+    check = false
+    (0...sorted_array.length - 1).each do |i|
+      next unless yield(sorted_array[i], sorted_array[i + 1]).positive?
+
+      sorted_array[i], sorted_array[i + 1] = sorted_array[i + 1], sorted_array[i]
+      check = true
     end
-    puts sorted_array
   end
+  puts sorted_array
+end
 
-  bubble_sort_by(%w[hi hello hey hi]) do |left, right|
-    left.length - right.length
-  end
+bubble_sort_by(%w[hi hello hey hi]) do |left, right|
+  left.length - right.length
+end
 
-unsorted_array = [11,5,7,6,15]
+unsorted_array = [11, 5, 7, 6, 15]
 bubble_sort(unsorted_array)
+
+# rubocop: enable Metrics/MethodLength
+# rubocop: enable Style/FrozenStringLiteralComment
