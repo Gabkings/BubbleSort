e1575cb69ea489e85494626282f810074e41c5da
dev <dev@localhost.localdomain>
2020-10-01 01:31:56 +0300

    Romoved unnesessay spaces

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,12 +3,9 @@
 def bubble_sort(array_to_sort)
     array_length = array_to_sort.size
     return array if array_length <= 1
-
     # create a loop to loop through array and compare elements
-
     loop do
         swapped = false
-
         # subtract one because Ruby arrays are zero-index based
         (array_length-1).times do |i|
             if array_to_sort[i] > array_to_sort[i+1]
@@ -16,15 +13,10 @@ def bubble_sort(array_to_sort)
                 swapped = true
             end
         end
-
-        break if not swapped
-            
+        break if not swapped 
     end
     puts array_to_sort
 end
-
-unsorted_array = [11,5,7,6,15]
-bubble_sort(unsorted_array)
 
 def bubble_sort_by(sorted_array)
     check = true
@@ -32,7 +24,6 @@ def bubble_sort_by(sorted_array)
       check = false
       (0...sorted_array.length - 1).each do |i|
         next unless yield(sorted_array[i], sorted_array[i + 1]).positive?
-  
         sorted_array[i], sorted_array[i + 1] = sorted_array[i + 1], sorted_array[i]
         check = true
       end
@@ -40,7 +31,9 @@ def bubble_sort_by(sorted_array)
     puts sorted_array
   end
 
-
   bubble_sort_by(%w[hi hello hey hi]) do |left, right|
     left.length - right.length
   end
+
+unsorted_array = [11,5,7,6,15]
+bubble_sort(unsorted_array)
