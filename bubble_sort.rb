56cbca8de0e86d3f017aaca80bbef13cb2c42555
dev <dev@localhost.localdomain>
2020-10-01 00:33:23 +0300

    Added a loop to compare array elements

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,4 +3,13 @@
 def bubble_sort(array_to_sort)
     array_length = array_to_sort.size
     return array if array_length <= 1
+
+    # create a loop to loop through array and compare elements
+
+    loop do
+        swapped = false
+
+        # subtract one because Ruby arrays are zero-index based
+        (array_length-1).times do |i|
+    end
 end
