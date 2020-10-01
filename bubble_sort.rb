e5154643b3f31f08e9de314d0ea524d9809bc888
dev <dev@localhost.localdomain>
2020-10-01 14:57:50 +0300

    Fixed rubocop errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,18 @@
-# rubocop: disable all
-
-def bubble_sort(array_to_sort)
-  array_length = array_to_sort.size
-  return array if array_length <= 1
+# bubble sort
+def bubble_sort(array)
+  array_length = array.size
+  array if array_length <= 1
   loop do
     swapped = false
     (array_length - 1).times do |i|
-      if array_to_sort[i] > array_to_sort[i + 1]
-        array_to_sort[i], array_to_sort[i + 1] = array_to_sort[i + 1], array_to_sort[i]
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
         swapped = true
       end
     end
     break unless swapped
   end
-  return array_to_sort
+  array
 end
 
 def bubble_sort_by(sorted_array)
@@ -22,11 +21,12 @@ def bubble_sort_by(sorted_array)
     check = false
     (0...sorted_array.length - 1).each do |i|
       next unless yield(sorted_array[i], sorted_array[i + 1]).positive?
+
       sorted_array[i], sorted_array[i + 1] = sorted_array[i + 1], sorted_array[i]
       check = true
     end
   end
-  return sorted_array
+  sorted_array
 end
 
 bubble_sort_by(%w[hi hello hey hi]) do |left, right|
@@ -35,5 +35,3 @@ end
 
 unsorted_array = [11, 5, 7, 6, 15]
 p bubble_sort(unsorted_array)
-
-# rubocop: enable all
